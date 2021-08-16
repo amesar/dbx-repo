052b9d5cbca9f8e284c8c33e1f7ae57ee90c674a
amesar <amesar>
2021-08-16 17:47:42 +0000

    test
    test

--- test.py
+++ test.py
@@ -1,10 +1,2 @@
-
-def print_versions():
-    import platform
-    print("Platform Version:",platform.python_version())
-
-    import sys
-    print("Sys Version:",sys.version)
-    print("Sys Version:",sys.version.replace("\n"," - "))
-
-#print_versions()
+# Databricks notebook source
+print("Hello test")
