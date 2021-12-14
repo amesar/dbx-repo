e3df4adcabbaf57891267362cc8d446fe99f7b34
amesar <amesar>
2021-12-14 17:17:59 +0000

    Added "Experiment name" widget

--- Test_Workspace_Experiment.py
+++ Test_Workspace_Experiment.py
@@ -3,12 +3,16 @@
 
 # COMMAND ----------
 
+notebook = dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get()
+dbutils.widgets.text("Experiment name", notebook)
+experiment_name = dbutils.widgets.get("Experiment name")
+experiment_name
+
+# COMMAND ----------
+
 import mlflow
 client = mlflow.tracking.MlflowClient()
-
-experiment_name = "/Repos/andre.mesarovic@databricks.com/dbx-repo/My_Workspace_Experiment"
 mlflow.set_experiment(experiment_name)
-
 
 # COMMAND ----------
 
@@ -23,7 +27,3 @@ with mlflow.start_run() as run:
 host_name = dbutils.notebook.entry_point.getDbutils().notebook().getContext().tags().get("browserHostName").get()
 uri = "https://{}/#mlflow/experiments/{}".format(host_name, run.info.experiment_id)
 displayHTML("""<b>Experiment URI:</b> <a href="{}">{}</a>""".format(uri,uri))
-
-# COMMAND ----------
-
-
