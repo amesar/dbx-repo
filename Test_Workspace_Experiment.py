89e5f616be379d7424a0a9b98b2a72625830ecee
amesar <amesar>
2022-04-04 19:51:24 +0000

    April update

--- Test_Workspace_Experiment.py
+++ Test_Workspace_Experiment.py
@@ -1,11 +1,14 @@
 # Databricks notebook source
 # MAGIC %md ### Test Repos - Workspace Experiment
+# MAGIC 
+# MAGIC Experiment: https://demo.cloud.databricks.com/#mlflow/experiments/12516221
 
 # COMMAND ----------
 
-notebook = dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get()
-dbutils.widgets.text("Experiment name", notebook)
-experiment_name = dbutils.widgets.get("Experiment name")
+#notebook = dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get()
+#dbutils.widgets.text("Experiment name", notebook)
+#experiment_name = dbutils.widgets.get("Experiment name")
+experiment_name = "/Users/andre.mesarovic@databricks.com/experiments/dbx-repo/Test_Workspace_Experiment"
 experiment_name
 
 # COMMAND ----------
@@ -17,7 +20,7 @@ mlflow.set_experiment(experiment_name)
 # COMMAND ----------
 
 with mlflow.start_run() as run:
-    mlflow.set_tag("greetings", "Hello Workspace Experiment")
+    mlflow.set_tag("greetings", "Hello Workspace Experiment - main branch")
     print("run_id:", run.info.run_id)
     print("experiment_id:", run.info.experiment_id)
     print("experiment_name:", client.get_experiment(run.info.experiment_id).name)
@@ -30,5 +33,7 @@ with mlflow.start_run() as run:
 # COMMAND ----------
 
 host_name = dbutils.notebook.entry_point.getDbutils().notebook().getContext().tags().get("browserHostName").get()
+uri = "https://{}/#mlflow/experiments/{}/runs/{}".format(host_name, run.info.experiment_id, run.info.run_id)
+displayHTML("""<b>Run URI:</b> <a href="{}">{}</a>""".format(uri,uri))
 uri = "https://{}/#mlflow/experiments/{}".format(host_name, run.info.experiment_id)
 displayHTML("""<b>Experiment URI:</b> <a href="{}">{}</a>""".format(uri,uri))
--- _README.py
+++ _README.py
@@ -4,6 +4,9 @@
 # MAGIC Notebooks
 # MAGIC * [Repo_Test]($Repo_Test)
 # MAGIC * [Test_Workspace_Experiment]($Test_Workspace_Experiment)
+# MAGIC * [Test_Notebook_Experiment]($Test_Notebook_Experiment)
 # MAGIC 
+# MAGIC Other
+# MAGIC * https://github.com/amesar/dbx-repo
 # MAGIC 
-# MAGIC Last Updated: 2021-12-13
+# MAGIC Last Updated: 2022-04-03
