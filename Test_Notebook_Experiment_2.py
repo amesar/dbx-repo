c8eae1a23b11f2efdf050507235e222d4b2c7fce
amesar <amesar>
2022-04-04 20:41:53 +0000

    main change

--- Test_Notebook_Experiment_2.py
+++ Test_Notebook_Experiment_2.py
@@ -8,7 +8,7 @@ client = mlflow.tracking.MlflowClient()
 
 # COMMAND ----------
 
-with mlflow.start_run(run_name="main") as run:
+with mlflow.start_run(run_name="main 2") as run:
     mlflow.set_tag("greetings", "Hello Notebook Experiment")
     print("run_id:", run.info.run_id)
     print("experiment_id:", run.info.experiment_id)
